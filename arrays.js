888f0f590fcc78b1090d72722d6b7ae7500ef159
Kevin Sage <ksagedeveloper@gmail.com>
2019-09-26 18:07:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ function addElementToBeginningOfArray(arr, candy) {
   arr.unshift(candy);
   return arr;
 }
+function destructivelyAddElementToBeginningOfArray(arr, candy) {
+  var moreCandy = [candy, ...arr]
+  return moreCandy;
+}
