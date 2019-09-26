f7eec51cf05eb423341b215d6b75f66d3753d447
Kevin Sage <ksagedeveloper@gmail.com>
2019-09-26 19:06:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,7 @@ function accessElementInArray(array, index) {
   return arr[index];
 }
 function removeElementFromBeginningOfArray(array) {
-  array.shift();
+  newArr = array.shift();
   return array;
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
